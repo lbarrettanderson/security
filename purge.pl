883646e526bf19d9521decb5a4689ff9124215fc
barrett <lbarrettanderson@gmail.com>
2017-06-22 21:28:29 -0700

    Fix purge

--- purge.pl
+++ purge.pl
@@ -6,13 +6,9 @@ $cam = $ARGV[0];
 # Keep this many days
 $days = $ARGV[1];
 
-
-my @abbr = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
-
-
 for ($i = $days + 10 ; $i > $days ; $i--) {
     ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time - $i * 60 * 60 * 24);
-    $location = "~/security/".$cam."/".($year + 1900)."/".$abbr[$mon]."/".sprintf("%02d", $mday);
+    $location = "~/security/".$cam."/".($year + 1900)."/".sprintf("%02d", ($mon + 1))."/".sprintf("%02d", $mday);
     $cmd = "rm -rf ".$location;
     #print "$cmd\n";
     `$cmd`;
